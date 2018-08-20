3e507c5eb99c313e43fac3f2fdb6e4e2cba612c4
MH-Lee <lmhoon012@gmail.com>
2018-08-20 21:41:41 +0900

    test 추가

--- tests/konlpy_tests.py
+++ tests/konlpy_tests.py
@@ -1,16 +1,35 @@
 import pytest
 
-from algorithms.nlp.kor_nlp import (
-    text_freq,
-    sentence_tokenize,
-    word_pos_tag,
-)
+from algorithms.nlp.kor_nlp import korean_text_pre
+from tests.expected_value import (
+    token_value,
+    count_value,
+    pos_value,
+    noun_value,
+    noun_count_value,
+    )
+##### korean NLP 테스트 #####
 
-##### English NLP 테스트 #####
+TEXT_SENT = '금융감독원이 삼성바이오로직스 회계처리 위반에 대한 재감리에 돌입했다.15일 금융당국에 따르면 금감원은 최근 재감리를 시작하고 최대한 빨리 진행하기로 했다. 특히 연말까지는 감리 조치안을 마무리한다는 방침이다.'
 
-TEXT_SENT = 'hello hello world world'
 
 ## Test 시작 ##
+def test_text_tokenize():
+    result = korean_text_pre(TEXT_SENT, 'token')
+    assert result == token_value
+
 def test_text_freq():
-    result = text_freq(TEXT_SENT)
-    assert result == [{'name': 'hello', 'y': 2}, {'name': 'world', 'y': 2}]
+    result = korean_text_pre(TEXT_SENT, 'count')
+    assert result == count_value
+
+def test_text_pos():
+    result = korean_text_pre(TEXT_SENT, 'pos')
+    assert result == pos_value
+
+def test_text_noun():
+    result = korean_text_pre(TEXT_SENT, 'noun')
+    assert result == noun_value
+
+def test_text_noun_value():
+    result = korean_text_pre(TEXT_SENT, 'noun_count')
+    assert result == noun_count_value
